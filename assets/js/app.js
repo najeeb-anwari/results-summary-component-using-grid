b2ba2ad4ea5e8fead7add0b47532b2d4dacd7e93
Najeeb Anwari <najeeb.anwari@gmail.com>
2023-09-30 12:51:05 +0430

    Loaded data from data.jason and displayed in view

---
+++ assets/js/app.js
@@ -0,0 +1,42 @@
+
+const testScore = document.querySelector(".test-scores");
+const jsonFilePath = '../../data.json';
+
+fetch(jsonFilePath)
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error(`Network response was not ok: ${response.statusText}`);
+    }
+    return response.json();
+  })
+  .then((jsonData) => {
+
+    
+    let obtainedScore = 0;
+    jsonData.forEach(element => {
+        
+        let testIcon = "../.." + element["icon"].slice(1);
+        obtainedScore += element["score"];
+        let html = 
+        `
+        <li class="${`${element["category"]}`.toLowerCase()}">
+          <div class="test">
+            <img src="${testIcon}" alt="${element["category"]} icon"> 
+            <span>${element["category"]}</span>
+          </div>
+          <div class="score">
+            <span class="obtained-score">${element["score"]}</span> <span class="total-score" >/ 100</span>
+          </div>
+        </li>
+        `;
+        testScore.innerHTML += html;
+    });
+    obtainedScore /= jsonData.length;
+    obtainedScore = obtainedScore.toFixed(0);
+    const averageObtainedScore = document.querySelector(".result .obtained-score");
+    averageObtainedScore.innerHTML = obtainedScore;
+    
+  })
+  .catch((error) => {
+    console.error(`Error fetching and parsing JSON: ${error.message}`);
+  });
