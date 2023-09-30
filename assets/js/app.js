a7362b6e266ac542ed79f24dac60ddd0dcfed33c
Najeeb Anwari <najeeb.anwari@gmail.com>
2023-09-30 13:19:08 +0430

    jsonPath changed

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,6 @@
 
 const testScore = document.querySelector(".test-scores");
-const jsonFilePath = './data.json';
+const jsonFilePath = '../../data.json';
 
 fetch(jsonFilePath)
   .then((response) => {
